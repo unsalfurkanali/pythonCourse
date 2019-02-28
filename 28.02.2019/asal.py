c234875bef1790d531e8e5d5e278b28986853301
Ali Ünsal <unsalfurkanali@users.noreply.github.com>
2019-03-01 00:06:17 +0300

    Update asal.py

--- 28.02.2019/asal.py
+++ 28.02.2019/asal.py
@@ -1,5 +1,6 @@
 sayi = input("Bir sayı giriniz : ")     #Sayı klavyeden okunuyor
 sayi = int(sayi)                        #str olarak alınınan sayı int'e çevriliyor
+a = 0
 for i in range(2, sayi-1, 1):           #i sayaç değişkeni 1 ile sayı-1 arasında her döngü sonunda 1 artır ve şu işlemleri tekrarla ;
     if sayi % i == 0:                   #Eğer sayı'nın i'ye bölümünden kalan 0'a eşitse
         a = a + 1                       #a'yı bir arttır
